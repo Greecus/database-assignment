a30c18b7d99ea1f8531bb252ed3eb81776709cbb
Krzysztof Jaszewski <KrzysztofJasz2002@gmail.com>
2024-02-18 01:40:26 +0100

    first commit

---
+++ databese_func.py
@@ -0,0 +1,113 @@
+import psycopg2
+
+DATABASE = """
+CREATE table Studenci(
+student_id int not null primary key,
+imie varchar(20),
+nazwisko varchar(20));
+
+CREATE table Wykladowcy(
+wykladowca_id int not null primary key,
+imie varchar(20),
+nazwisko varchar(20));
+
+create table Przedmioty(
+przedmiot_id int not null primary key,
+przedmiot_nazwa varchar(20) not null,
+wykladowca int not null,
+FOREIGN key (wykladowca) REFERENCES Wykladowcy(wykladowca_id) on delete cascade on update cascade);
+
+create table Grupy(
+grupa_id int not null primary key,
+przedmiot_id int,
+FOREIGN KEY (przedmiot_id) REFERENCES Przedmioty(przedmiot_id)
+	on delete cascade
+	on update cascade);
+
+create table Oceny(
+ocena_id int not null primary key,
+przedmiot_id int,
+student int,
+data DATE not null,
+FOREIGN KEY (przedmiot_id) REFERENCES Przedmioty(przedmiot_id)
+	on delete cascade
+	on update cascade,
+foreign key (student) references Studenci(student_id)
+	on delete cascade
+	on update cascade);
+	
+create table StudenciWGrupach(
+grupa_id int,
+student_id int,
+FOREIGN KEY (student_id) REFERENCES Studenci(student_id)
+	on delete cascade
+	on update cascade,
+FOREIGN KEY (grupa_id) REFERENCES Grupy(grupa_id)
+	on delete cascade
+	on update cascade);
+"""
+
+def delete_all_tables():
+    delete_tables_sql = """
+        drop table StudenciWGrupach;
+
+        drop table Oceny;
+
+        drop table Grupy;
+
+        drop table Przedmioty;
+
+        drop table Wykladowcy;
+
+        drop table Studenci;"""
+    
+    connection_params = {
+        'dbname': 'postgres',
+        'user': 'postgres',
+        'password': 'password',
+        'host': 'localhost',
+        'port': '5432'}
+    
+    
+    with psycopg2.connect(**connection_params) as conn:
+        cur = conn.cursor()
+        try:
+            cur.execute(delete_tables_sql)
+        except psycopg2.errors.UndefinedTable:
+            pass
+
+
+def create_tables():
+    connection_params = {
+        'dbname': 'postgres',
+        'user': 'postgres',
+        'password': 'password',
+        'host': 'localhost',
+        'port': '5432'}
+    
+    with psycopg2.connect(**connection_params) as conn:
+        cur = conn.cursor()
+        cur.execute(DATABASE)
+
+def insert_data_to_db(students_data, teachers_data, subjects_data, groups_data, marks_data, students_in_groups_data):
+    connection_params = {
+        'dbname': 'postgres',
+        'user': 'postgres',
+        'password': 'password',
+        'host': 'localhost',
+        'port': '5432'}
+    
+    with psycopg2.connect(**connection_params) as conn:
+        cur = conn.cursor()
+        
+        cur.executemany("INSERT INTO Studenci (student_id, imie, nazwisko) VALUES (%s, %s, %s)", students_data)
+
+        cur.executemany("INSERT INTO Wykladowcy (wykladowca_id, imie, nazwisko) VALUES (%s, %s, %s)", teachers_data)
+
+        cur.executemany("INSERT INTO Przedmioty (przedmiot_id, przedmiot_nazwa, wykladowca) VALUES (%s, %s, %s)", subjects_data)
+
+        cur.executemany("INSERT INTO Grupy (grupa_id, przedmiot_id) VALUES (%s, %s)", groups_data)
+
+        cur.executemany("INSERT INTO Oceny (ocena_id, przedmiot_id, student, data) VALUES (%s, %s, %s, %s)", marks_data)
+
+        cur.executemany("INSERT INTO StudenciWGrupach (grupa_id, student_id) VALUES (%s, %s)", students_in_groups_data)
---
+++ main.py
@@ -0,0 +1,61 @@
+from faker import Faker
+from random import randint, choices, shuffle
+from databese_func import create_tables, delete_all_tables, insert_data_to_db
+
+def gen_fake_data():
+    example_subjects = ["Polski", "Matematyka", "Biologia", "Chemia", "Historia", "WOS", "Fizyka", "WF"]
+
+    fake = Faker("pl_PL")
+
+    n_students = randint(30, 50)
+    n_subjects = randint(5, 8)
+    n_teacher = randint(3, 5)
+    groups_per_subject = 3
+    max_marks_per_student = 20
+
+    students_data = [(id, fake.first_name(), fake.last_name()) for id in range(1,n_students+1)]
+
+    teachers_data = [(id, fake.first_name(), fake.last_name()) for id in range(1,n_teacher+1)]
+
+    teacher_ids_list = list(range(1,n_teacher+1))
+    shuffle(teacher_ids_list)
+    teacher_ids_list = teacher_ids_list * 3
+    subjects_data = [(id, subject, teacher_id) 
+                     for id, subject, teacher_id 
+                     in zip(
+                         range(1,n_subjects+1),
+                         choices(example_subjects, k=n_subjects), 
+                         teacher_ids_list)]
+    
+    groups_data = []
+    group_index = 1
+    for subject_id in range(1,n_subjects+1):
+        for _ in range(groups_per_subject):
+            groups_data.append((group_index,subject_id))
+            group_index += 1
+    
+    marks_data = []
+    mark_index = 1
+    for n in range(1,n_students+1):
+        for _ in range(randint(4,max_marks_per_student)):
+            marks_data.append((mark_index, randint(1,n_subjects), n, fake.date()))
+            mark_index += 1
+
+    students_in_groups_data = []
+    for student_id in range(1,n_students+1):
+        for subject_id in range(n_subjects):
+            group_id = subject_id * groups_per_subject + student_id%groups_per_subject+1
+            students_in_groups_data.append((group_id,student_id))
+
+    print("")
+    return students_data, teachers_data, subjects_data, groups_data, marks_data, students_in_groups_data
+
+
+
+if __name__ == "__main__":
+    delete_all_tables()
+    create_tables()
+
+    students_data, teachers_data, subjects_data, groups_data, marks_data, students_in_groups_data = gen_fake_data()
+    insert_data_to_db(students_data, teachers_data, subjects_data, groups_data, marks_data, students_in_groups_data)
+    
